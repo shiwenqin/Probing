6f8b4d256213b75e4709bf30776c193ca9982976
Qinswt <carlosqsw@outlook.com>
2024-02-19 17:32:48 +0000

    update preprocessing scripts

--- preprocessing/ontonotes.py
+++ preprocessing/ontonotes.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import logging as log
+from random import randint
 
 log.basicConfig(level=log.INFO)
 
 class PreprocessOntonotes():
 
-    def __init__(self, path):
+    def __init__(self, path, is_train=False):
 
         self.path = path
+        self.is_train = is_train
 
         self.data = self._load_dataset()
 
@@ -43,22 +45,41 @@ class PreprocessOntonotes():
         df.loc[:,'mapped_named_entities'] = df['named_entities'].apply(lambda x: _mapping_ner_lst(x))
         df = df.drop(['named_entities'], axis=1)
 
+        df.loc[:,'pos_tags'] = df['pos_tags'].apply(list)
+
+        if self.is_train:
+            df = df.drop_duplicates(subset=['sentence'], keep='first')
+        
+        df = self._get_pos_label(df)
+
         log.info('Preprocessing completed.')
         log.info('Preprocessed dataset shape: {}'.format(df.shape))
+        return df
+    
+    def _get_pos_label(self, df):
+
+        def _choose_pos(pos_tags):
+            rand = randint(0, len(pos_tags)-1)
+            return rand, pos_tags[rand]
+        
+        df.loc[:, 'pos_index'], df.loc[:, 'pos_label'] = zip(*df.pos_tags.apply(_choose_pos))
+
         return df
     
     def save(self, path, df):
 
         log.info('Saving preprocessed Ontonotes dataset...')
-        df.to_csv(path)
+        df.to_csv(path, index=False)
+        df.to_parquet(path.replace('.csv', '.parquet'), index=False)
         log.info('Dataset saved at {}'.format(path))
         return
     
 if __name__ == '__main__':
+    # test validation
 
-    path = '../dataset/ontonotesv5_english_v12_train.parquet'
-    save_path = '../dataset/ontonotesv5_english_v12_train_processed.csv'
-    preprocess = PreprocessOntonotes(path)
+    path = '../dataset/ontonotesv5_english_v12_test.parquet'
+    save_path = '../dataset/ontonotesv5_english_v12_test_processed.csv'
+    preprocess = PreprocessOntonotes(path, is_train=False)
     df = preprocess.preprocess()
     preprocess.save(save_path, df)
     print(df.head())
