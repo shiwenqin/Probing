878b593265c8a327f81688967bce96b1a9bdeb37
shiwenqin <carlosqsw@outlook.com>
2024-02-21 14:39:20 +0000

    add batch size as input

--- src/encoding/encode.py
+++ src/encoding/encode.py
@@ -18,7 +18,8 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Input Configuration
 parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--base_model', type=str, default='bert-base-uncased', help='Base model to use')
+parser.add_argument('-m', '--base_model', type=str, default='bert-base-uncased', help='Base model to use')
+parser.add_argument('-b', '--batch_size', type=int, default=32, help='Batch size for encoding data')
 args = parser.parse_args()
 
 # Model Configuration
@@ -30,7 +31,7 @@ TRAIN_FILE = '../../dataset/ontonotesv5_english_v12_train_processed.parquet'
 # VAL_FILE = '../../dataset/ontonotesv5_english_v12_validation_processed.parquet'
 # TEST_FILE = '../../dataset/ontonotesv5_english_v12_test_processed.parquet'
 
-BATCH_SIZE = 32
+BATCH_SIZE = args.batch_size
 
 # Load Models
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
@@ -47,7 +48,7 @@ max_len_data = max(train_df['sentence'].apply(lambda x: len(x.split())))
 # Encode Data
 bert_encoding = {}
 for i in range(LAYER_NUM):
-    bert_encoding[i] = torch.tensor([])
+    bert_encoding[i] = []
 
 model.eval()
 for batch in tqdm(np.array_split(train_df, len(train_df) // BATCH_SIZE), desc='Training'):
@@ -57,16 +58,16 @@ for batch in tqdm(np.array_split(train_df, len(train_df) // BATCH_SIZE), desc='T
                        max_length = max_len_data).to(device)
     outputs = model.forward(**inputs, output_hidden_states=True)
     for i in range(LAYER_NUM):
-        bert_encoding[i] = torch.cat((bert_encoding[i], outputs.hidden_states[i].detach().cpu()), dim=0)
+        bert_encoding[i].append(outputs.hidden_states[i].detach().cpu().numpy())
     # for obj in gc.get_objects():
     #     count = 0
     #     if torch.is_tensor(obj):
     #         count += 1
     # print(count)
 
-print(bert_encoding[0].shape)
-print(bert_encoding[1].shape)
-print(train_df.shape)
+# print(bert_encoding[0].shape)
+# print(bert_encoding[1].shape)
+# print(train_df.shape)
 
 with open('bert_encoding_train.pkl', 'wb') as f:
     pickle.dump(bert_encoding, f)
