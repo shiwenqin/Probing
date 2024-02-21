fbc37bbe6b88ffffa90eb255531eebc36bc0df6f
shiwenqin <carlosqsw@outlook.com>
2024-02-21 14:27:32 +0000

    add codes to pre-compute bert embeddings

--- src/encoding/encode.py
+++ src/encoding/encode.py
@@ -1,6 +1,7 @@
 import pickle
 import argparse
 import warnings
+import gc
 import logging as log
 from tqdm import tqdm
 
@@ -22,18 +23,50 @@ args = parser.parse_args()
 
 # Model Configuration
 BASE_MODEL = args.base_model
+LAYER_NUM = 13 # 12 hidden layers + 1 embedding layer
 
 # Data Configuration
 TRAIN_FILE = '../../dataset/ontonotesv5_english_v12_train_processed.parquet'
-VAL_FILE = '../../dataset/ontonotesv5_english_v12_validation_processed.parquet'
-TEST_FILE = '../../dataset/ontonotesv5_english_v12_test_processed.parquet'
+# VAL_FILE = '../../dataset/ontonotesv5_english_v12_validation_processed.parquet'
+# TEST_FILE = '../../dataset/ontonotesv5_english_v12_test_processed.parquet'
+
+BATCH_SIZE = 32
 
 # Load Models
 tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL)
 model = AutoModel.from_pretrained(BASE_MODEL).to(device)
 
 # Load Data
-train_df = pd.read_parquet(TRAIN_FILE, columns=['sentence', 'pos_index', 'pos_label'])
-val_df = pd.read_parquet(VAL_FILE, columns=['sentence', 'pos_index', 'pos_label'])
-test_df = pd.read_parquet(TEST_FILE, columns=['sentence', 'pos_index', 'pos_label'])
+train_df = pd.read_parquet(TRAIN_FILE, columns=['sentence'])
+# val_df = pd.read_parquet(VAL_FILE, columns=['sentence'])
+# test_df = pd.read_parquet(TEST_FILE, columns=['sentence'])
 
+max_len_data = max(train_df['sentence'].apply(lambda x: len(x.split())))
+# print(max_len_data)
+
+# Encode Data
+bert_encoding = {}
+for i in range(LAYER_NUM):
+    bert_encoding[i] = torch.tensor([])
+
+model.eval()
+for batch in tqdm(np.array_split(train_df, len(train_df) // BATCH_SIZE), desc='Training'):
+    batch = batch.reset_index(drop=True)
+    inputs = tokenizer(batch['sentence'].tolist(), return_tensors='pt', 
+                       padding='max_length', add_special_tokens=False, 
+                       max_length = max_len_data).to(device)
+    outputs = model.forward(**inputs, output_hidden_states=True)
+    for i in range(LAYER_NUM):
+        bert_encoding[i] = torch.cat((bert_encoding[i], outputs.hidden_states[i].detach().cpu()), dim=0)
+    # for obj in gc.get_objects():
+    #     count = 0
+    #     if torch.is_tensor(obj):
+    #         count += 1
+    # print(count)
+
+print(bert_encoding[0].shape)
+print(bert_encoding[1].shape)
+print(train_df.shape)
+
+with open('bert_encoding_train.pkl', 'wb') as f:
+    pickle.dump(bert_encoding, f)
